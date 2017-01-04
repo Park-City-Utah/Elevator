eccbabc87663269afbbf657d5332e3ae8fe41f48
Park-City-Utah <parkcity.pf@gmail.com>
2017-01-03 22:49:24 -0800

    Elevator program started

---
+++ Elevator/Elevator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my elevator program");
+            string x = Console.ReadLine();
+        }
+    }
+}
